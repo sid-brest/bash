801cafbe087147ae1dce2d8e39c6bf9633b471f6
sid-brest <siiid@mail.ru>
2022-02-19 00:05:14 +0300

    19022022

--- Homework1_BashScripting_Task3-1.sh
+++ Homework1_BashScripting_Task3-1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-grep -i "dolore" ./lorem
+echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." > lorem
+if grep "dolore" ./lorem; then
+  echo "The word 'dolore' found"
+fi
