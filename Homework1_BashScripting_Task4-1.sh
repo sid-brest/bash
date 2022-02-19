9d8cff8604b79c261dc179c4037ec33291c7e93d
sid-brest <siiid@mail.ru>
2022-02-19 17:25:43 +0300

    task4final

--- Homework1_BashScripting_Task4-1.sh
+++ Homework1_BashScripting_Task4-1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-HomeDir="$HOME"
-UserName="$(whoami)"
-UserID="$(id -u)"
-GroupInfo=
-Term=
-CurDir=
-DateTime=
+HomeDir=$HOME
+UserName=$(whoami)
+UserID=$(id -u)
+GroupInfo=$(id | awk '{print $2}' | sed -e's/gid=//g')
+Term=$(tty --version | sed '1!d' | awk '{print $3}' | sed -e's/)//g')
+CurDir=$(pwd)
+DateTime=$(LANG=en_us_88591; date -u)
 
 echo "Home dir: $HomeDir"
 echo "User Name: $UserName"
-echo "User ID: $UserID"
+echo "User ID: $UserID($UserName)"
 echo "Group Information: $GroupInfo"
 echo "Terminal: $Term"
 echo "Current directory: $CurDir"
