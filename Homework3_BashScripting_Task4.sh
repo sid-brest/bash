5d43ad2140f00e0543aa8a467a97c6d42abe5dee
sid-brest <siiid@mail.ru>
2022-02-22 00:57:25 +0300

    HW3_Task4

--- Homework3_BashScripting_Task4.sh
+++ Homework3_BashScripting_Task4.sh
@@ -1,7 +1,8 @@
-# Write a script that executes the command cat /etc/shadow. If the command return a 0 exit status, report Command succeeded and exit with a 0 exit status. If the command returns a non-zero exit status, report Command failed and exit with a 1 exit status.
-# Expected output:
-# [devops@localhost ~]$ ./03-get-shadow.sh
-# Command failed
-# [devops@localhost ~]$ sudo ./03-get-shadow.sh
-# Command succeeded
-#!/bin/bash
+cat /etc/shadow &> /dev/null
+status=$?
+if test $status -eq 0
+then
+	echo "Command succeeded"
+else
+	echo "Command failed"
+fi
