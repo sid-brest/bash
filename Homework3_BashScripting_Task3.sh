81025de710256d97a91cf25b226fba27f2ee3a9e
sid-brest <siiid@mail.ru>
2022-02-21 15:50:48 +0300

    HW3_Task3

--- Homework3_BashScripting_Task3.sh
+++ Homework3_BashScripting_Task3.sh
@@ -1,10 +1,10 @@
-# Develop a script called 03-which-daemon.sh that checks if the httpd and init daemons are running on your system. If an httpd is running, the script should print a message like, "This machine is running a web server." Use ps to check on processes
-#  Example of script execution:
-# [devops@localhost ~]$ ps -ef | grep [h]ttpd
-# apache   27906 29214  0 Sep29 ?    00:00:00 /usr/sbin/httpd -DFOREGROUND
-# root     29214     1  0 Sep27 ?    00:00:35 /usr/sbin/httpd -DFOREGROUND
-# [devops@localhost ~]$
-# [devops@localhost ~]$ ./03-which-daemon.sh
-# This machine is running a web server.
-# [devops@localhost ~]$
+# Use <sudo systemctl stop\start httpd> to stop\start service
 #!/bin/bash
+StatusCheck=$(ps -ef | grep [h]ttpd])
+HttpdSearch="httpd"
+if [[ "$StatusCheck" == *"$HttpdSearch"* ]] ; then
+    echo "This machine is running a web server."
+else
+    echo "This machine is NOT running a web server."
+fi
+echo $StatusCheck
