62894dee65a6f9e979b7c83e0c96ee4edeebbb45
sid-brest <siiid@mail.ru>
2022-02-21 15:56:23 +0300

    HW3_Task3v2

--- Homework3_BashScripting_Task3.sh
+++ Homework3_BashScripting_Task3.sh
@@ -1,10 +1,9 @@
 # Use <sudo systemctl stop\start httpd> to stop\start service
 #!/bin/bash
-StatusCheck=$(ps -ef | grep [h]ttpd])
+StatusCheck=$(ps -ef | grep [h]ttpd)
 HttpdSearch="httpd"
 if [[ "$StatusCheck" == *"$HttpdSearch"* ]] ; then
     echo "This machine is running a web server."
 else
     echo "This machine is NOT running a web server."
 fi
-echo $StatusCheck
