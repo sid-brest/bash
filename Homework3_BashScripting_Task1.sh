bb4c4c5168464472b4fe908f98340145df0ffbcc
sid-brest <siiid@mail.ru>
2022-02-21 12:59:59 +0300

    HW3_Task1

--- Homework3_BashScripting_Task1.sh
+++ Homework3_BashScripting_Task1.sh
@@ -1,6 +1,18 @@
-# Develop a script that prints information about the current month. The script should print the number of days of the current month.
-# Expected output:
-# [devops@localhost ~]$ ./month-days.sh
-# 31
 #!/bin/bash
 leanYearMonths=(31 28 31 30 31 30 31 31 30 31 30 31)
+CurDate=$(date +%m)
+case $CurDate in
+    01) Num=0 ;;
+    02) Num=1 ;;
+    03) Num=2 ;;
+    04) Num=3 ;;
+    05) Num=4 ;;
+    06) Num=5 ;;
+    07) Num=6 ;;
+    08) Num=7 ;;
+    09) Num=8 ;;
+    10) Num=9 ;;
+    11) Num=10 ;;
+    12) Num=11 ;;
+esac
+echo ${leanYearMonths[$Num]}
