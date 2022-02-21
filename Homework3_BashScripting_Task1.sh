501ffb3b885bf6fcd8f62e4d63430e7cdac662d7
sid-brest <siiid@mail.ru>
2022-02-21 14:28:18 +0300

    HW3_Task1_Task2

--- Homework3_BashScripting_Task1.sh
+++ Homework3_BashScripting_Task1.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 leanYearMonths=(31 28 31 30 31 30 31 31 30 31 30 31)
-CurDate=$(date +%m)
-case $CurDate in
-    01) Num=0 ;;
-    02) Num=1 ;;
-    03) Num=2 ;;
-    04) Num=3 ;;
-    05) Num=4 ;;
-    06) Num=5 ;;
-    07) Num=6 ;;
-    08) Num=7 ;;
-    09) Num=8 ;;
-    10) Num=9 ;;
-    11) Num=10 ;;
-    12) Num=11 ;;
-esac
+CurDate=$(LANG=en_us_88591;date | awk '{print $2 }')
+if [[ "$CurDate" = "Jan" ]] ; then 
+    Num=0
+elif [[ "$CurDate" = "Feb" ]] ; then
+    Num=1
+elif [[ "$CurDate" = "Mar" ]] ; then 
+    Num=2
+elif [[ "$CurDate" = "Apr" ]] ; then 
+    Num=3
+elif [[ "$CurDate" = "May" ]] ; then 
+    Num=4
+elif [[ "$CurDate" = "Jun" ]] ; then 
+    Num=5
+elif [[ "$CurDate" = "Jul" ]] ; then 
+    Num=6
+elif [[ "$CurDate" = "Aug" ]] ; then 
+    Num=7
+elif [[ "$CurDate" = "Sep" ]] ; then 
+    Num=8
+elif [[ "$CurDate" = "Oct" ]] ; then 
+    Num=9
+elif [[ "$CurDate" = "Nov" ]] ; then 
+    Num=10
+elif [[ "$CurDate" = "Dec" ]] ; then 
+    Num=11
+fi
 echo ${leanYearMonths[$Num]}
--- Homework3_BashScripting_Task2.sh
+++ Homework3_BashScripting_Task2.sh
@@ -1,2 +1,18 @@
-# Do the same, using a case statement and an alternative use of the date command.
 #!/bin/bash
+leanYearMonths=(31 28 31 30 31 30 31 31 30 31 30 31)
+CurDate=$(date +%m)
+case $CurDate in
+    01) Num=0 ;;
+    02) Num=1 ;;
+    03) Num=2 ;;
+    04) Num=3 ;;
+    05) Num=4 ;;
+    06) Num=5 ;;
+    07) Num=6 ;;
+    08) Num=7 ;;
+    09) Num=8 ;;
+    10) Num=9 ;;
+    11) Num=10 ;;
+    12) Num=11 ;;
+esac
+echo ${leanYearMonths[$Num]}
