71e653a7efa71f22e9c8951c5425045a1eded2d6
sid-brest <siiid@mail.ru>
2022-02-23 00:48:55 +0300

    HW3_Task5v1

--- Homework3_BashScripting_Task5.sh
+++ Homework3_BashScripting_Task5.sh
@@ -5,9 +5,13 @@
  function rand() {
     echo $(($RANDOM % ${1}))
 }
- 
+
 for item in `seq 1 10`
 do
     value="$(rand 100)"
     echo "[$item] => ${value}"
-done
+done | \
+( mapfile; printf "%s" "${MAPFILE[@]}" )
+echo "min=$min"
+echo "max=$max"
+echo "sum=$sum"
