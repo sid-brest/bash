a63de944c30439f79c0a1bcf305210791b9db551
sid-brest <siiid@mail.ru>
2022-02-23 21:50:05 +0300

    HW3_Task5Final

--- Homework3_BashScripting_Task5.sh
+++ Homework3_BashScripting_Task5.sh
@@ -1,18 +1,28 @@
-# min=10
-# max=95
-# sum=539
 #!/bin/bash
  function rand() {
     echo $(($RANDOM % ${1}))
 }
-
 for item in `seq 1 10`
 do
     value="$(rand 100)"
     echo "[$item] => ${value}"
 done | \
-( mapfile; printf "%s" "${MAPFILE[@]}" )
-# sed -i 's/[*] => //g'
+# Read array from pipe and save to temp file
+(mapfile OUTPUTARRAY; printf "%s" "${OUTPUTARRAY[@]}") > tmp1
+cat tmp1
+# Leave only the numbers in the array
+cat tmp1 | cut -d ' ' -f3 > tmp2
+readarray -t NUMBERARRAY < tmp2
+min=${NUMBERARRAY[0]}
+max=${NUMBERARRAY[0]}
+sum=0
+for i in "${NUMBERARRAY[@]}"; do
+  (( i > max )) && max=$i
+  (( i < min )) && min=$i
+#   Let zdd each item to the total count
+  let sum+=$i
+done
 echo "min=$min"
 echo "max=$max"
 echo "sum=$sum"
+rm tmp1 tmp2
