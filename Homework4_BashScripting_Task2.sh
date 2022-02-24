494e502fdf69fc228efefbef0e84e94f4cce1e77
sid-brest <siiid@mail.ru>
2022-02-24 12:56:28 +0300

    HW4_Task2

--- Homework4_BashScripting_Task2.sh
+++ Homework4_BashScripting_Task2.sh
@@ -1,5 +1,2 @@
-# Develop simple script which will be print numbers in range from 20 to -20 with step=4. (
-# Hint: try to use echo {80..100}; echo {80..100..3})
-# Expected output:
-#  20 16 12 8 4 0 -4 -8 -12 -16 -20
 #!/bin/bash
+echo {20..-20..4}
