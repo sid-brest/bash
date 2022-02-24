51d9c147a667f50e7011de2fbf2d65e9c0741aa8
sid-brest <siiid@mail.ru>
2022-02-24 12:41:56 +0300

    HW4_Task1

--- Homework4_BashScripting_Task1.sh
+++ Homework4_BashScripting_Task1.sh
@@ -1,7 +1,3 @@
-# Develop simple script which will be print numbers in range from 1 to 10. 
-# (Hint: try to use seq 10 20 with different options)
-# Expected output:
-#  1 2 3 4 5 6 7 8 9 10
 #!/bin/bash
 echo {1..10}
 seq -s ' ' 1 10
