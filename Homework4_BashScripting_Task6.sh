3e8d0aeab6ff141461a79987c7be4af1676a6f5c
sid-brest <siiid@mail.ru>
2022-02-26 00:39:48 +0300

    HW4_Task6v0

--- Homework4_BashScripting_Task6.sh
+++ Homework4_BashScripting_Task6.sh
@@ -1,19 +1,2 @@
-# You’re given the file with varied paths. 
-# Depend on existing “/” at the end of each line create regular file 
-# (path without ‘/’) or directory (path with / at the end) with given path.
-# Expected output:
-
-# [devops@localhost ~]$ cat paths
-# /tmp/file1
-# /tmp/file2
-# /tmp/dir1/
-# /tmp/dir3/
-# /tmp/dir3/file3
-# [devops@localhost ~]$ cat paths | ./04-create-items.sh
-# [devops@localhost ~]$ ls –l /tmp
-# /tmp/dir1/
-# /tmp/dir3/
-# /tmp/file1
-# /tmp/file2
-# ...
 #!/bin/bash
+echo -e "/tmp/file1\n/tmp/file2\n/tmp/dir1/\n\n/tmp/dir3/\n\n/tmp/dir3/file3\n" > paths
