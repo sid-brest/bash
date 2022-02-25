35d4cbc2c29d319c542f319c6c1c7b30c09fe39f
sid-brest <siiid@mail.ru>
2022-02-26 00:11:25 +0300

    HW4_Task5v0

--- Homework4_BashScripting_Task5.sh
+++ Homework4_BashScripting_Task5.sh
@@ -1,21 +1,2 @@
-# Let’s develop a simple console game! You’re given a script template (see below). 
-# Append script which will be:
-# At the beginning of the game script randomly sets Magic number
-# User is running script and trying to guess magic number:
-# if entered number less than magic number, script outputs less; 
-# if entered number greater than magic number, script outputs greater; 
-# if user have guessed the magic number, script outputs You win!
-# Expected output:
-
-# [devops@localhost ~]$ cat 03-magic-number.sh
-# #!/bin/bash
-# magicNumber=$(( $RANDOM % 100 ))
-# ## write your code here ##
-# [devops@localhost ~]$ ./03-magic-number.sh
-# 10
-# less
-# 20
-# greater
-# 15
-# You win!
 #!/bin/bash
+magicNumber=$(( $RANDOM % 100 ))
