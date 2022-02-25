d4146909b23f9c56d9e5434c6f9b026a9386e4a9
sid-brest <siiid@mail.ru>
2022-02-25 23:38:01 +0300

    HW4_Task3

--- Homework4_BashScripting_Task3.sh
+++ Homework4_BashScripting_Task3.sh
@@ -1,17 +1,11 @@
-# You are given an array of numbers. Using only one loop perform the following tasks:
-# Print only even numbers
-# Don't print numbers in range [30, 60]
-# Stop loop executing when number will be greater than 85
-# Expected output:
-# 2
-# 4
-# 6
 #!/bin/bash
 array=$(seq 1 100)
 for item in ${array}; do
-    for [ $item -lt 30 ]; do
-        if [ $(expr $item % 2) == 0 ] && [ $item -le 85 ]; then
-          echo $item
-        fi
-    done
+  if [ $(expr $item % 2) -eq 0 ] && [ $item -lt 30 ]; then
+    echo $item
+  elif [ $(expr $item % 2) -eq 0 ] && [ $item -gt 60 ]; then
+    echo $item
+  elif [ $item -gt 85 ]; then
+    break
+  fi
 done
