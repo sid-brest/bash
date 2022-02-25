6e596f520420772adc911c37d3a90f89a7b94728
sid-brest <siiid@mail.ru>
2022-02-26 00:08:07 +0300

    HW4_Task4

--- Homework4_BashScripting_Task4.sh
+++ Homework4_BashScripting_Task4.sh
@@ -1,13 +1,6 @@
-# Develop simple script which will be execute infinite while loop with reading input in variable 
-# (etc. var) (read command) and outputting message Hello ${var}. 
-# The has been ended when we enter word stop
-# Expected output:
-
-# [devops@localhost ~]$ ./04-infinite.sh
-# Andrey
-# Hello, Andrey
-# Aliens
-# Hello, Aliens
-# stop
-# [devops@localhost ~]$
 #!/bin/bash
+read var
+while [ $var != "stop" ]; do
+    echo "Hello, ${var}"
+    read var
+done
