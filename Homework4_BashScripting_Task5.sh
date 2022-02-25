9f4bd3ce4d2b0f25054e39e74fd92560ebf60dee
sid-brest <siiid@mail.ru>
2022-02-26 00:29:02 +0300

    HW4_Task5

--- Homework4_BashScripting_Task5.sh
+++ Homework4_BashScripting_Task5.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 magicNumber=$(( $RANDOM % 100 ))
+while :
+do
+    read userinput
+    if [ $userinput -lt $magicNumber ]; then
+        echo "less"
+    elif [ $userinput -gt $magicNumber ]; then
+        echo "greater"
+    else
+        echo "You win!"
+    fi
+done
