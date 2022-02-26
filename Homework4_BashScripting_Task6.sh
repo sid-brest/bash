73231ae1ae3063faf9e25b6eed95ad2bf75b6949
sid-brest <siiid@mail.ru>
2022-02-26 15:17:45 +0300

    HW4_Tas

--- Homework4_BashScripting_Task6.sh
+++ Homework4_BashScripting_Task6.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+rm -rf /tmp/file1 /tmp/file2 /tmp/dir1 /tmp/dir3 /tmp/dir3/file3
 echo -e "/tmp/file1\n/tmp/file2\n/tmp/dir1/\n\n/tmp/dir3/\n\n/tmp/dir3/file3\n" > paths
+sed -i '/^$/d' paths
+while read -r line
+do
+    mkdir $line
+done
