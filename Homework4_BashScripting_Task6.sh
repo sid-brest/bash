943e5581c0c124c9735068cfd7ddb9e894150b3d
sid-brest <siiid@mail.ru>
2022-02-27 02:47:58 +0300

    HW

--- Homework4_BashScripting_Task6.sh
+++ Homework4_BashScripting_Task6.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 # use next row to remove directories if they were created
-# rm -rf /tmp/file1 /tmp/file2 /tmp/dir1 /tmp/dir3 /tmp/dir3/file3
-
+rm -rf /tmp/file1 /tmp/file2 /tmp/dir1 /tmp/dir3 /tmp/dir3/file3
 # generate file "paths"
-# echo -e "/tmp/file1\n/tmp/file2\n/tmp/dir1/\n\n/tmp/dir3/\n\n/tmp/dir3/file3\n" > paths
-
-sed -i '/^$/d' paths
+echo -e "/tmp/file1\n/tmp/file2\n/tmp/dir1/\n\n/tmp/dir3/\n\n/tmp/dir3/file3\n" > paths
 while read -r line
 do
-    mkdir -v $line
+    if [ -z "${line}" ]; then
+        continue
+    else
+        mkdir $line
+    fi
 done
