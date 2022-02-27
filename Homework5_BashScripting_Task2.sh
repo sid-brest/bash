ed5e596f66e020b9d7482d78db332b45480ee8db
sid-brest <siiid@mail.ru>
2022-02-27 14:57:35 +0300

    HW5_Task2

--- Homework5_BashScripting_Task2.sh
+++ Homework5_BashScripting_Task2.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+partycheck() {
+    if [ $1 -lt 18 ] && [[ $2 == "yes" ]]; then
+        echo "You may go but must be back before midnight."
+    elif [ $1 -ge 18 ]; then
+        echo "You may go to the party."
+    else 
+        echo "You cannot go to the party."
+    fi
+}
+partycheck $1 $2
