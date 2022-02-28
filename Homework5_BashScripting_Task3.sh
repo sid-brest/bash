b978251c6a9c5c03799cf3acacd4d5b4b3157fef
sid-brest <siiid@mail.ru>
2022-02-28 12:36:19 +0300

    HW5_Task3

--- Homework5_BashScripting_Task3.sh
+++ Homework5_BashScripting_Task3.sh
@@ -2,13 +2,13 @@
 dirfileinfo() {
     fileslist=( $( ls "$1" ) )
     for item in ${fileslist[@]}; do
-        name=$(stat $1$item)
-        type=$(stat $1$item | sed -n '2'p | awk '{print $8 " " $9}')
-        permissions=$(stat $1$item | sed -n '4'p | awk '{print $2}')
-            echo $name | awk '{print $1 " " $2}'
-            echo "Type: $type" 
-            echo "Permissions: $permissions" 
-            echo "-------"
+        name=$(stat $1/$item)
+        type=$(stat $1/$item | sed -n '2'p | awk '{print $8 " " $9}')
+        permissions=$(stat $1/$item | sed -n '4'p | awk '{print $2}')
+                echo $name | awk '{print $1 " " $2}'
+                echo "Type: $type" 
+                echo "Permissions: $permissions" 
+                echo "-------"            
     done 
 }
 dirfileinfo $1
