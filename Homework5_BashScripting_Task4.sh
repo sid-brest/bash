b63eedd7a1b09ff13a81c2086d777c81b84eff70
sid-brest <siiid@mail.ru>
2022-02-28 21:16:36 +0300

    HW5_Task

--- Homework5_BashScripting_Task4.sh
+++ Homework5_BashScripting_Task4.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+mathfunc() {
+    amount=0
+    product=1
+    for args in $@; do
+        let amount+=$args
+        ((product*=$args))
+    done
+    echo "Sum: $amount"
+    echo "Prod: $product"
+}
+mathfunc $@
