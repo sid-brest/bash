d1f46842d789d3a8e8cfec63ff432332a5a4d954
sid-brest <siiid@mail.ru>
2022-03-01 11:20:02 +0300

    HW5_Task7v0

--- Homework5_BashScripting_Task7.sh
+++ Homework5_BashScripting_Task7.sh
@@ -1,1 +1,12 @@
+# Script should print its’ full path and its’ name
+# Expected output:
+
+# [devops@localhost ~]$ ./script.sh
+# /home/devops/script.sh
+# script.sh
+# [devops@localhost ~]$ cd /tmp
+# [devops@localhost tmp/]$ /home/devops/script.sh
+# /home/devops/script.sh
+# script.sh
+
 #!/bin/bash
