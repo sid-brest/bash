ee52ae9395d74fb0afaf7c990db9ea16d8c2e071
sid-brest <siiid@mail.ru>
2022-03-02 02:19:48 +0300

    HW5_Task7

--- Homework5_BashScripting_Task7.sh
+++ Homework5_BashScripting_Task7.sh
@@ -1,12 +1,5 @@
-# Script should print its’ full path and its’ name
-# Expected output:
-
-# [devops@localhost ~]$ ./script.sh
-# /home/devops/script.sh
-# script.sh
-# [devops@localhost ~]$ cd /tmp
-# [devops@localhost tmp/]$ /home/devops/script.sh
-# /home/devops/script.sh
-# script.sh
-
 #!/bin/bash
+GetFileDir=$(dirname ${BASH_SOURCE[0]})
+GetFileName=$(basename ${BASH_SOURCE[0]})
+echo "$(readlink -f "$GetFileDir")/$GetFileName"
+echo $GetFileName
