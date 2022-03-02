622b29541d7bd7cfed85278c6276c88a9336c65c
sid-brest <siiid@mail.ru>
2022-03-03 00:46:05 +0300

    FT1_v1

--- Final_Homework1_BashScripting.sh
+++ Final_Homework1_BashScripting.sh
@@ -2,8 +2,8 @@
 cat accounts.csv | awk '
     BEGIN {OFS=FS=","};
     NR>1{
-        $column5 = tolower($5);
-        split($column5, names, " ");
+        $5 = tolower($5);
+        split($5, names, " ");
         for(i in names)
             sub(names[i], toupper(substr(names[i], 1, 1))substr(names[i], 2), $5)
     }1' > accounts_new.csv
