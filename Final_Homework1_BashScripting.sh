383a882b5a5ef790d5f8526641b74220c1f28d0f
sid-brest <siiid@mail.ru>
2022-03-10 23:21:04 +0300

    FT1_emails

--- Final_Homework1_BashScripting.sh
+++ Final_Homework1_BashScripting.sh
@@ -3,9 +3,11 @@ cat accounts.csv | awk 'BEGIN {OFS=FS=","};
    NR > 1 {
         split($5, names, " ")
         $7 = tolower($5"@abc.com")
-        split($7, email, " ")
-        for (i in names) 
+        split($7, email, ".")
+        for (i in names) {
             sub(names[i], toupper(substr(names[i], 1, 1))substr(names[i], 2), $5)
-        for (i in email)
-            sub(email[i], substr(email[i]%% *, 1, 1)substr(email[i]##* ,(index(email[i]," ")+1)), $7)
-    }; 1' > accounts_new.csv
+        }
+        for (m in email) {
+            sub(email[m], substr(email[m], 1, 1)gensub(/.*\ /, "", "g", substr(email[m], 2)), $7)
+        }
+   }; 1' > accounts_new.csv
