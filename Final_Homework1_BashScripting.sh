9bf2724809f3bfa1e10caf1d6ad60f14bf323565
sid-brest <siiid@mail.ru>
2022-03-14 14:31:33 +0300

    FT1_v14032022

--- Final_Homework1_BashScripting.sh
+++ Final_Homework1_BashScripting.sh
@@ -4,14 +4,17 @@ cat accounts.csv | awk 'BEGIN {OFS=FS=","};
    NR > 1 {
     # Divide string into pieces separated by fieldsep to array
         split($5, names, " ")
-        $7 = tolower($5"@abc.com")
+        $7 = tolower($5)
     # Select the array value as original string
         split($7, email)
         for (i in names) {
             sub(names[i], toupper(substr(names[i], 1, 1))substr(names[i], 2), $5)
         }
-        for (m in email) {
-    # Replace from 2nd string index till space with nothing 
-            sub(email[m], substr(email[m], 1, 1)gensub(/.*\ /, "", "g", substr(email[m], 2)), $7)
+        if ($7!~/@/) {
+            for (m in email) {
+                sub(email[m], substr(email[m], 1, 1)gensub(/.*\ /, "", "g", substr(email[m], 2)), $7)
             }
+        }
+        # ($7 in array) ? $7 = $7array[$7]"@abc.com" : $7 = $7"@abc.com"; 
+        # array[$7]++
     }; 1' > accounts_new.csv
