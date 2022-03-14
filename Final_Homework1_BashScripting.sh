e590d0e2af79eb80369b0e30f26f3e99a60086c3
sid-brest <siiid@mail.ru>
2022-03-15 00:04:50 +0300

    FT1_15032022

--- Final_Homework1_BashScripting.sh
+++ Final_Homework1_BashScripting.sh
@@ -10,11 +10,11 @@ cat accounts.csv | awk 'BEGIN {OFS=FS=","};
             # Select the array value as original string
             split($7, email)
         }
-    # To upper first letters
+    # Toupper first letters
         for (i in names) {
             sub(names[i], toupper(substr(names[i], 1, 1))substr(names[i], 2), $5)
         }
-    # Leave name first letter & remove all strings before last name
+    # Leave name first letter & remove strings before last name
         for (m in email) {
                 sub(email[m], substr(email[m], 1, 1)gensub(/.*\ /, "", "g", substr(email[m], 2)), $7)
         }
