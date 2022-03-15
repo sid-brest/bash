a6315d8682acb520ea80038625b9d39ee6cc384a
sid-brest <siiid@mail.ru>
2022-03-15 09:49:34 +0300

    FT1_15032022

--- Final_Homework1_BashScripting.sh
+++ Final_Homework1_BashScripting.sh
@@ -7,17 +7,17 @@ cat accounts.csv | awk 'BEGIN {OFS=FS=","};
     # Check if the user has email, and if not, add
         if ($7!~/@/) {
             $7 = tolower($5)"@abc.com"
-            # Select the array value as original string
-            split($7, email)
         }
     # Toupper first letters
         for (i in names) {
             sub(names[i], toupper(substr(names[i], 1, 1))substr(names[i], 2), $5)
         }
+    # Select the array value as original string
+            split($7, email)
     # Leave name first letter & remove strings before last name
         for (m in email) {
                 sub(email[m], substr(email[m], 1, 1)gensub(/.*\ /, "", "g", substr(email[m], 2)), $7)
-        }
-    # Check duplicates and add number after username 
-        ($7 in array) ? sub(/@/, array[$7]"@", $7) : $7; array[$7]++
+    # Check duplicates and add number after username
+                (email[m] in array) ? sub(/@/, array[email[m]]"@", $7) : $7; array[email[m]]++
+        }        
 }; 1' > accounts_new.csv
