59c5549efd139e7bedaa904ede7ece67768249cb
sid-brest <siiid@mail.ru>
2022-03-23 15:09:15 +0300

    FH2_23032022

--- FH2/ZabbixVM/zagent_install.sh
+++ FH2/ZabbixVM/zagent_install.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+!/bin/bash
+sudo yum -y -config -manager --enable rhel-7-server-optional-rpms
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
-sudo yum update
+sudo yum -y update && yum -y upgrade 
+sudo setenforce 0
+sudo yum -y install zabbix-server-mysql zabbix-web-mysql zabbix-agent
+zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u zabbix -p zabbix
+sudo systemctl restart zabbix-server zabbix-agent
+sudo systemctl enable zabbix-server zabbix-agent
