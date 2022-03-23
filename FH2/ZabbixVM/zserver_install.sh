418711f8cd9ce6bc4f03393d896f88d337344684
sid-brest <siiid@mail.ru>
2022-03-24 02:20:27 +0300

    FH2_24032022

--- FH2/ZabbixVM/zserver_install.sh
+++ FH2/ZabbixVM/zserver_install.sh
@@ -2,7 +2,6 @@
 # disable SELinux on CentOS
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
-
 # install & update packages
 sudo yum-config-manager --enable rhel-7-server-optional-rpms
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
