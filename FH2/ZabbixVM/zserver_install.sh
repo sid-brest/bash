a7a812e2dcdf9a9fc8c44461f9595bb47cdd7ce5
sid-brest <siiid@mail.ru>
2022-03-29 01:06:20 +0300

    zserver_install

--- FH2/ZabbixVM/zserver_install.sh
+++ FH2/ZabbixVM/zserver_install.sh
@@ -6,6 +6,7 @@ sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
 
 # install & update packages
 sudo yum -y update
+sudo yum -y install nano
 sudo yum -y install httpd
 sudo systemctl enable httpd
 sudo systemctl restart httpd
@@ -28,3 +29,13 @@ mysql -uroot -pzabbix -e "CREATE DATABASE zabbix character set utf8 collate utf8
 mysql -uroot -pzabbix -e "CREATE USER 'zabbix'@'localhost' identified by 'zabbixpass'"
 mysql -uroot -pzabbix -e "GRANT all privileges on zabbix.* to 'zabbix'@'localhost'"
 mysql -uroot -pzabbix -e "QUIT"
+
+# configure zabbix
+zcat /usr/share/doc/mariadb-server-mysql*/create.sql.gz | mysql -uzabbix -pzabbixpass --database=zabbix
+echo "DBPassword=zabbix" | sudo tee -a /etc/zabbix/zabbix_server.conf
+sudo systemctl start zabbix-server
+sudo systemctl enable zabbix-server
+echo "php_value[date.timezone] = Europe/Minsk" | sudo tee -a /etc/opt/rh/rh-php72/php-fpm.d/zabbix.conf
+sudo setsebool -P httpd_can_connect_zabbix on
+sudo systemctl enable rh-php72-php-fpm
+sudo systemctl restart zabbix-server httpd rh-php72-php-fpm
