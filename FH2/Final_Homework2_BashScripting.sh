b74bd9d9817363929e2d1d8ebaa7047ff5e6c778
sid-brest <siiid@mail.ru>
2022-03-29 00:17:07 +0300

    FH2_29032022

--- FH2/Final_Homework2_BashScripting.sh
+++ FH2/Final_Homework2_BashScripting.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# delete vagrant boxes on your system:
+# vagrant box list | cut -f 1 -d ' ' | xargs -L 1 vagrant box remove -f
 (cd ./ZabbixVM && vagrant up)
-(cd ./ZabbixVM && vagrant vbguest zserver)
-(cd ./ZabbixVM && vagrant ssh zserver)
+(cd ./ZabbixVM && vagrant vbguest)
--- FH2/ZabbixVM/zserver_install.sh
+++ FH2/ZabbixVM/zserver_install.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
+
 # disable SELinux
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
+
 # install & update packages
-sudo yum-config-manager --enable rhel-7-server-optional-rpms
-sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
 sudo yum -y update
-sudo yum -y install wget nano httpd zabbix-server-mysql mariadb-server 
-# start\enable services
-sudo systemctl start httpd.service
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
+sudo yum -y install httpd
+sudo systemctl enable httpd
+sudo systemctl restart httpd
+sudo rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
+sudo yum install -y zabbix-server-mysql zabbix-get
+sudo yum-config-manager --enable zabbix-frontend
+sudo yum -y install centos-release-scl
+sudo yum -y install zabbix-web-mysql-scl zabbix-apache-conf-scl
+sudo yum -y install mariadb-server
+sudo systemctl enable --now mariadb
+
+# create initial database
+mysql -uroot -e "UPDATE mysql.user SET Password = PASSWORD('zabbix') WHERE User = 'root'"
+sudo systemctl restart mariadb
+mysql -uroot -pzabbix -e "DROP USER ''@'localhost'"
+mysql -uroot -pzabbix -e "DROP USER ''@'$(hostname)'"
+mysql -uroot -pzabbix -e "DROP DATABASE test"
+mysql -uroot -pzabbix -e "FLUSH PRIVILEGES"
+mysql -uroot -pzabbix -e "CREATE DATABASE zabbix character set utf8 collate utf8_bin"
+mysql -uroot -pzabbix -e "CREATE USER 'zabbix'@'localhost' identified by 'zabbixpass'"
+mysql -uroot -pzabbix -e "GRANT all privileges on zabbix.* to 'zabbix'@'localhost'"
+mysql -uroot -pzabbix -e "QUIT"
