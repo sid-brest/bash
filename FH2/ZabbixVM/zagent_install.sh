81764938931d30daae312e94fe460b549e776534
sid-brest <siiid@mail.ru>
2022-03-30 01:24:03 +0300

    zagent_correction

--- FH2/ZabbixVM/zagent_install.sh
+++ FH2/ZabbixVM/zagent_install.sh
@@ -11,6 +11,7 @@ sudo rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.
 sudo yum install -y zabbix-agent
 
 # configure zabbix
+sudo sed -i 's/127.0.0.1/192.168.56.10/g' /etc/zabbix/zabbix_agentd.conf
 sudo systemctl start zabbix-agent
 sudo systemctl enable zabbix-agent
 sudo systemctl restart zabbix-agent
