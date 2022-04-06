40070e157b67aec1ec299330b2fdf60c2c5f74e1
sid-brest <siiid@mail.ru>
2022-04-07 01:52:54 +0300

    FH4_backup_final

--- FH4/backup.sh
+++ FH4/backup.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-tar -zcvf "$name.tar.gz" code
+(cd ./NFS && vagrant ssh client -c "
+    sudo yum -y install tar;
+    sudo tar -zcvf /mnt/nfs/var/nfsshare/$(date "+%d-%m-%Y-%T").tar.gz /root /home;
+    echo 'sudo tar -zcvf /mnt/nfs/var/nfsshare/\$(date "+%d-%m-%Y-%T").tar.gz /root /home/' | sudo tee /vagrant/backup.sh;
+    echo '0 0 * * * vagrant /vagrant/backup.sh >/dev/null 2>&1' | sudo tee /vagrant/backup.sh;
+    crontab -r;
+    crontab -l | { cat; echo '0 0 * * * vagrant /vagrant/backup.sh >/dev/null 2>&1'; } | crontab -;
+")
