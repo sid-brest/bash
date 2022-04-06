6ecc81cb6aa92514539eac936d7951d4e866b0a0
sid-brest <siiid@mail.ru>
2022-04-06 14:21:50 +0300

    FH4_06042022

--- FH4/provision.sh
+++ FH4/provision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # delete vagrant boxes on your system:
 # vagrant box list | cut -f 1 -d ' ' | xargs -L 1 vagrant box remove -f
-(cd ./NFS && vagrant up)
+(cd ./NFS && vagrant up server --provision)
+(cd ./NFS && vagrant up client --provision)
 (cd ./FNS && vagrant vbguest)
