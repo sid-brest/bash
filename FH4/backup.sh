4d87f3bb3871a1734a3a11bd80a2998b09a0f137
sid-brest <siiid@mail.ru>
2022-04-07 09:26:37 +0300

    FH4_Final

--- FH4/backup.sh
+++ FH4/backup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 (cd ./NFS && vagrant ssh client -c "
     sudo yum -y install tar;
+# create tar gz & push it to shared folder on server
     sudo tar -zcvf /mnt/nfs/var/nfsshare/$(date "+%d-%m-%Y-%T").tar.gz /root /home;
+# create shell script with tar gz task
     echo 'sudo tar -zcvf /mnt/nfs/var/nfsshare/\$(date "+%d-%m-%Y-%T").tar.gz /root /home/' | sudo tee /vagrant/backup.sh;
-    echo '0 0 * * * vagrant /vagrant/backup.sh >/dev/null 2>&1' | sudo tee /vagrant/backup.sh;
+# remove all tasks in planner     
     crontab -r;
+# create daily backup task at 0:00 in planner
     crontab -l | { cat; echo '0 0 * * * vagrant /vagrant/backup.sh >/dev/null 2>&1'; } | crontab -;
 ")
--- FH4/provision.sh
+++ FH4/provision.sh
@@ -3,13 +3,16 @@ whatinstall() {
   read -p "$1"": " input
     if [[ $input == "server" ]]; then
         (cd ./NFS && vagrant up server --provision)
+        (cd ./NFS && vagrant reload server --provision)
     elif [[ $input == "client" ]]; then
         (cd ./NFS && vagrant up client --provision)
+        (cd ./NFS && vagrant reload client --provision)
     else
         echo "Please type 'server' or 'client'" 
     fi
 }
 whatinstall "Select what to configure"
+# сheck if blank answer is sent
    if [ -z $input ]; then
         echo "Parameter is empty. Restart the shell script"
    fi 
